11f5611d6626e2541ffcbe8e74ba587e8fc21406
William Pereira <william.pereira@escriba.com.br>
2020-11-10 13:48:59 -0300

    Orientação à Objeto

---
+++ 02 - JavaScript ES6 essencial/orientação-objeto/class/class.js
@@ -0,0 +1,26 @@
+'use strict';
+
+class Animal {
+  constructor() {
+    this.qtdePatas = 0;
+  }
+  movimentar() {}
+}
+
+class Cachorro extends Animal {
+  constructor(morde) {
+    super();
+    this.qtdePatas = 4;
+    this.morde = morde;
+  }
+
+  latir() {
+    console.log('Au! Au!');
+  }
+}
+
+const pug = new Cachorro(false);
+const pitbull = new Cachorro(true);
+
+console.log(pug);
+console.log(pitbull);
